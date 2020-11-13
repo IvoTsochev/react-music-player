fea4d115e885dbaca29333dbb8ee789c4c72247b
IvoTsochev <ivo.tsochev@live.com>
2020-11-14 00:35:08 +0200

    setting up the components, style them and import the array of songs from chillhop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles/app.css";
+// importing components
+import Player from "./components/Player";
+import Song from "./components/Song";
+// importing util
+import data from "./util";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
